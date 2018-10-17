40918aaca2be4f9eb1a08a67bb50dc86d74e65b9
juan pisani <juanquipisani99@gmail.com>
2018-10-17 11:33:32 -0300

    latitud and longitud interpolate done

--- test.py
+++ test.py
@@ -2,24 +2,43 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import interpolate as ip
 
-def test():
+
+def interpolateLongitud():
     headers = ['Altura', 'Latitud', 'Longitud']
     data = pd.read_csv("coordenadas.csv", names=headers)
-    # aux = list()
-    # for i in data['Altura']:
-    #     hello = np.interp(i, data["Latitud"], data['Altura'])
-    #     aux.append(hello)
-    # print(aux)
-    aux = list()
-    for i in data['Altura']:
-        aux.append(data['Altura'].get(i))
-    print("hello")
-    asd = np.interp(1000000, data['Altura'], data['Latitud'])
-    print(asd)
-    # plt.plot(data['Latitud'], data['Altura'])
-    # plt.show()
+    newLong = []
+    newAlt = []
+    i = 1000
+    while i <= 50000:
+        fp = data['Longitud']
+        xp = data['Altura']
+        y_inter = ip.interp1d(xp, fp)
+        newLong.append(y_inter(i))
+        newAlt.append(i)
+        i += 10
+    plt.plot(newLong, newAlt)
+    plt.show()
+
+
+def interpolateLatitud():
+    headers = ['Altura', 'Latitud', 'Longitud']
+    data = pd.read_csv("coordenadas.csv", names=headers)
+    newLat = []
+    newAlt = []
+    i = 1000
+    while i <= 50000:
+        fp = data['Latitud']
+        xp = data['Altura']
+        y_inter = ip.interp1d(xp, fp)
+        newLat.append(y_inter(i))
+        newAlt.append(i)
+        i += 10
+    plt.plot(newLat, newAlt)
+    plt.show()
 
 
 if __name__ == '__main__':
-    test()
+    # interpolateLongitud()
+    interpolateLatitud()
